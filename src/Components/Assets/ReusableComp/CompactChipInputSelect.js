09bc167a2ed81c6546f6a96117231fdf29097fad
Priyom Saha <712020:cf107986-cfaa-4891-b456-e773e05fcf92@bots.bitbucket.org>
2023-09-17 18:55:06 +0530

    Added reusable component for Compact Chip Input Selections

---
+++ src/Components/Assets/ReusableComp/CompactChipInputSelect.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import {
+  Box,
+  Chip,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from "@mui/material";
+
+function CompactChipInputSelect({
+  totalList,
+  selectedItems,
+  setSelectedItems,
+  inputLabelText,
+}) {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const getStyles = (selected, theme) => {
+    return {
+      backgroundColor:
+        selectedItems.indexOf(selected) === -1 ? "white" : "#a5bfe3",
+      fontWeight:
+        selectedItems.indexOf(selected) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightBold,
+    };
+  };
+
+  const theme = useTheme();
+  const count = 2;
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setSelectedItems(typeof value === "string" ? value.split(",") : value);
+  };
+
+  // Handler for removing a selected item when a chip is deleted
+  const handleDelete = (itemToDelete) => {
+    const newSelectedItems = selectedItems.filter(
+      (item) => item !== itemToDelete
+    );
+    setSelectedItems(newSelectedItems);
+  };
+
+  return (
+    <>
+      <FormControl sx={{ width: "100%" }} size="small">
+        <InputLabel id="demo-multiple-chip-label">{inputLabelText}</InputLabel>
+        <Select
+          multiple
+          value={selectedItems}
+          onChange={handleChange}
+          input={<OutlinedInput label={`"${inputLabelText}"`} />}
+          renderValue={(selected) => (
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5, py: 0.5 }}>
+              {selected.map((value, index) =>
+                index < count ? (
+                  <Chip size="small" key={value} label={value} />
+                ) : null
+              )}
+              {selected.length > count
+                ? `& ${selected.length - count} more`
+                : null}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {totalList.map((item) => (
+            <MenuItem key={item} value={item} style={getStyles(item, theme)}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+}
+
+export default CompactChipInputSelect;
--- src/Components/Navigations/Merchants/Merchant/AddEditMerchant/OrderType.js
+++ src/Components/Navigations/Merchants/Merchant/AddEditMerchant/OrderType.js
@@ -1,92 +1,17 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+import CompactChipInputSelect from "Components/Assets/ReusableComp/CompactChipInputSelect";
 
 const types = ["Delivery", "Pickup", "Custom"];
 
-function getStyles(type, orderType, theme) {
-  return {
-    fontWeight:
-      orderType.indexOf(type) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
 export default function OrderType() {
-  const theme = useTheme();
   const [orderType, setOrderType] = React.useState([]);
 
-  const count = 2; //Count of chips to show in one line
-
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setOrderType(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
-
   return (
-    <div>
-      <FormControl sx={{ width: "100%" }} size="small">
-        <InputLabel id="demo-multiple-chip-label">
-          Select Order Type*
-        </InputLabel>
-        <Select
-          multiple
-          value={orderType}
-          onChange={handleChange}
-          input={<OutlinedInput label="Select Order Type*" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5, py: 0.5 }}>
-              {selected.map((value, index) =>
-                index < count ? (
-                  <Chip
-                    size="small"
-                    key={value}
-                    label={value}
-                    onDelete={"delete"}
-                  />
-                ) : null
-              )}
-              {selected.length > count
-                ? `& ${selected.length - count} more`
-                : null}
-            </Box>
-          )}
-          MenuProps={MenuProps}
-        >
-          {types.map((type) => (
-            <MenuItem
-              key={type}
-              value={type}
-              style={getStyles(type, orderType, theme)}
-            >
-              {type}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+    <CompactChipInputSelect
+      totalList={types}
+      selectedItems={orderType}
+      setSelectedItems={setOrderType}
+      inputLabelText="Select Order Type*"
+    />
   );
 }
